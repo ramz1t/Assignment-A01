f7c3c83423809063f3448efc716a7211eca0f07d
Timur Ramazanov <75879061+ramz1t@users.noreply.github.com>
2025-01-05 22:57:48 +0100

    fix Dice

--- task_01/Dice.java
+++ task_01/Dice.java
@@ -4,20 +4,25 @@ class Dice {
     private int sides;
     private int lastRoll;
     private String[] diceGraphics = { "⚀", "⚁", "⚂", "⚃", "⚄", "⚅" };
+    private static Random random = new Random();
 
     Dice(int sides) {
+        if (sides < 0) sides = 1;
         this.sides = sides;
     }
 
     public int roll() {
-        Random random = new Random();
         lastRoll = random.nextInt(sides) + 1;
         return lastRoll;
     }
 
     @Override
     public String toString() {
-        return diceGraphics[lastRoll - 1];
+        if (lastRoll > diceGraphics.length) {
+            return "[" + lastRoll + "]";
+        } else {
+            return diceGraphics[lastRoll - 1];
+        }
     }
 
     public int getValue() {
